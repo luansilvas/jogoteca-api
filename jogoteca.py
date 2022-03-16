cbeccb44b0798ccdecda81891bef61086a20c813
Luan Santos <luanssantos12@gmail.com>
2022-03-16 14:33:43 -0300

    adding session validation to game registration

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'secretpass'
 
 
 class Jogo:
@@ -10,17 +11,58 @@ class Jogo:
         self.console = console
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario','Ação','SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
 
-    lista = [jogo1,jogo2, jogo3]
+lista = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='jogos', jogos=lista)
+
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html',titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+    return render_template('novo.html', titulo='Novo Jogo')
 
-app.run()
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+
+    lista.append(jogo)
+
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso')
+        return redirect('/')
+    else:
+        flash('Tente novamente!')
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado')
+    return redirect('/')
+
+
+app.run(debug=True)
